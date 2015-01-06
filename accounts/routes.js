8b36a4d96e7260cccfa9412e0d018c7e01289641
Jeremy Meyer <jjman505@gmail.com>
2015-01-05 21:57:38 -0500

    users api

--- accounts/routes.js
+++ accounts/routes.js
@@ -18,3 +18,46 @@ Router.route('/accounts/login', function() {
 }, {
   name: 'accounts.login'
 });
+
+// Users Public API
+
+Router.route('/api/users/:_id', {where: 'server', name: 'api/users'})
+  .get(function() {
+    var _id = this.params._id;
+    var user = Meteor.users.findOne({ _id: _id });
+    sendJson(this.response, { success: true, data: user });
+  })
+  .put(function() {
+    var _id = this.params._id;
+    var user = this.request.body;
+    var obj = {
+      emails: [ {address: user.email} ],
+      profile: user.profile
+    }
+    Meteor.users.update({ _id: _id }, { $set: obj });
+    if(user.password) Accounts.setPassword(_id, user.password);
+    sendJson(this.response, { success: true });
+  })
+  .delete(function() {
+    var _id = this.params._id;
+    var res = this.response;
+    var user = Meteor.users.remove({ _id: _id }, function(err) {
+      if(err) sendError(res, err);
+      sendJson(res, { success: true });
+    });
+  });
+
+Router.route('/api/users', {where: 'server'})
+  .post(function() {
+    var user = this.request.body;
+    var res = this.response;
+    Accounts.createUser({
+      email: user.email,
+      password: user.password,
+      profile: {
+        name: user.name
+      }
+    }, function() {
+      sendJson(res, { success: true });
+    });
+  });
--- server/lib/helpers.js
+++ server/lib/helpers.js
@@ -1,3 +1,19 @@
+
+sendError = function(res, err) {
+  res.statusCode = 500;
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+
+  if(typeof err === 'object' || typeof err === 'array')
+    err = JSON.stringify(err);
+  else
+    err = err.toString();
+
+  res.end(err);
+};
+
 sendJson = function(res, obj) {
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
